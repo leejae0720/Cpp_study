a2847638b5fa112a70b9f88e5ae35c71560a2e6c
Jae-Hong, Lee <90753251+JaeHongLeee@users.noreply.github.com>
2023-07-28 15:00:25 +0900

    Add files via upload

--- baekjoon_num_5635.cpp
+++ baekjoon_num_5635.cpp
@@ -1,146 +1,26 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
 using namespace std;
 
-int a,s;
-string name[20];
-string output_name[2];
-int day, month, year;
-int maxday, minday, maxmonth, minmonth, maxyear, minyear, max_i, min_i;
-int lastday, lastmonth, lastyear;
+int n;
+int main(int argc, char **argv){
+  std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
 
-void caculation_function(int n){
+  cin >> n;
+  vector<pair<pair<int, int>, pair<int, string>> > student(n); 
 
-    for(int i = 0; i < n; i++){
-        cin >> name[i] >> day >> month >> year;
+  for(int i=0; i<n; i++){
+    cin >> student[i].second.second >> student[i].second.first >> 
+    student[i].first.second >> student[i].first.first;
+  }
 
-        if(i = 0){
-            lastyear = year;
-            lastmonth = month;
-            lastday = day;
-        }
+  sort(student.begin(), student.end());
 
-        if(year <= 2010 && year >= 1990 && day <= 31 && day >= 1 && month >= 1 && month <= 12){
-            switch(n){
-                case 1:
-                    output_name[0] = name[i];
-                    output_name[1] = name[i];
-                    cout << output_name[0] << "\n" << output_name[1];
-                    break;
-                
-                case 2:
-                    if(lastyear > year){
+  cout << student[n-1].second.second << "\n" << student[0].second.second;
 
-                    }
-                    else if(lastyear < year){
-
-                    }
-                    else if(lastyear == year){
-                        if(lastmonth > month){
-
-                        }
-                        else if(lastmonth < month){
-
-                        }
-                        else if(lastmonth == month){
-                            if(lastday > day){
-
-                            }
-                            else if(lastday < day){
-
-                            }
-                            else if(lastday == day){
-                                cout << "input error" << endl;
-                            }
-                        }
-                    }
-                    
-
-            }
-        }
-
-        else{
-            cout << "input error" <<endl;
-            break;
-        }
-    }
-}  
-/*
-    if(n >= 1 && n <= 100){
-        lastyear = 2010;
-        for (int i = 0; i < n; i++){
-            cin >> name[i] >> day >> month >> year;
-
-            if(year <= 2010 && year >= 1990 && day <= 31 && day >= 1 && month >= 1 && month <= 12){
-                if(lastyear < year){
-                    minyear = year;
-                    lastyear = year;
-                    lastmonth = month;
-                    lastday = day;
-                    output_name[0] = name [i];
-                }
-
-                else if(lastyear > year){
-                    maxyear = year;
-                    lastyear = year;
-                    lastmonth = month;
-                    lastday = day;
-                    output_name[1] = name[i];
-                }
-
-                else if(lastyear == year){
-                    if(lastmonth > month){
-                        maxmonth = month;
-                        lastmonth = month;
-                        lastday = day;
-                        output_name[1] = name[i];
-                    }
-
-                    else if(lastmonth < month){
-                        lastmonth = month;
-                        lastday = day;
-                    }
-                    
-                    else if(lastmonth == month){
-                        if(lastday > day){
-                            lastday = day;
-                            output_name[1] = name[i];
-                        }
-
-                        else if(lastday < day){
-                            minday = day;
-                            lastday = day;
-                        }
-
-                        else if(lastday == day){
-                            break;
-                        }
-                    }
-                }
-            }
-
-            else{
-                cout << "input error" << endl;
-                break;
-            }
-        }
-
-    cout << output_name[0] << endl;
-    cout << output_name[1] << endl;
-    }
-
-    if(n < 1 || n > 100){
-        cout << "input error" << endl;
-    }
-}
-*/
-
-int main(){
-    cin >> a;
-    if(a >= 1 || a <= 100){
-        caculation_function(a);
-    }
-    else{
-        cout << "input error" << endl;
-    }
-    return 0;
+  return 0;
 }
