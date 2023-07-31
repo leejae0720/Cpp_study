490a2c36ca6713fb90133b1398b8c97046badd79
Jae-Hong, Lee <90753251+JaeHongLeee@users.noreply.github.com>
2023-07-31 11:43:04 +0900

    Add files via upload

---
+++ baekjoon_study/baekjoon_num_1330.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int A, B;
+int main(){
+    cin >> A >> B;
+    if(A > B){
+        cout << ">";
+    }
+    else if(A < B){
+        cout << "<";
+    }
+    else if(A == B){
+        cout << "==";
+    }
+    
+    return 0;
+}
